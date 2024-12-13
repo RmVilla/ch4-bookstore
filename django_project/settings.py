bcc8a0bcd8aa4de593fed003b1017a1420427b02
Rodmark "RM" Villa <rmvilla987@gmail.com>
2024-12-13 12:02:15 +0800

    'ch5'

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # local
     "accounts.apps.AccountsConfig",
+    "pages.apps.PagesConfig",
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
